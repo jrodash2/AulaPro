7faa58fb111b95f72d4d3b70d0d37e587f9e1c77
jrodash2 <61074808+jrodash2@users.noreply.github.com>
2026-02-24 21:18:25 -0600

    Corrige extends de templates a empleados/base.html

--- upcv_app/empleados_app/forms.py
+++ upcv_app/empleados_app/forms.py
@@ -1,59 +1,82 @@
 from django import forms
-from .models import Empleado, ConfiguracionGeneral, Grado
-from django.forms import CheckboxInput, DateInput
+
+from .models import Carrera, ConfiguracionGeneral, Empleado, Establecimiento, Grado, Matricula
 
 
-class ConfiguracionGeneralForm(forms.ModelForm):
+class BaseRihoForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            if isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                current = field.widget.attrs.get("class", "")
+                field.widget.attrs["class"] = f"{current} form-control".strip()
+
+
+class ConfiguracionGeneralForm(BaseRihoForm):
     class Meta:
         model = ConfiguracionGeneral
-        fields = ['nombre_institucion', 'nombre_institucion2', 'direccion', 'logotipo', 'tel', 'sitio_web', 'correo']
-        
-    # Personalizamos la clase 'form-control' para otros campos si es necesario
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        # Añadimos 'form-control' a los campos, si no está especificado en los widgets
-        for field in self.fields.values():
-            if 'class' not in field.widget.attrs:
-                field.widget.attrs['class'] = 'form-control'
+        fields = ["nombre_institucion", "nombre_institucion2", "direccion", "logotipo", "tel", "sitio_web", "correo"]
 
-class EmpleadoForm(forms.ModelForm):
+
+class EmpleadoForm(BaseRihoForm):
     class Meta:
         model = Empleado
-        fields = ['nombres', 'apellidos', 'grado', 'imagen', 'tel']
-
-    # Personalizar los campos para agregar la clase 'form-control'
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        # Agregar la clase 'form-control' a todos los campos del formulario
-        for field in self.fields.values():
-            field.widget.attrs['class'] = field.widget.attrs.get('class', '') + ' form-control'
-        
-        # Si hay algún campo relacionado, como "grado", se personaliza aquí
-        self.fields['grado'].queryset = Grado.objects.all()  # Se asegura de que solo se muestren los grados disponibles.
-
-
-
-class EmpleadoEditForm(forms.ModelForm):
-    class Meta:
-        model = Empleado
-        fields = ['nombres', 'apellidos', 'grado', 'imagen', 'tel', 'activo']
-        labels = {'activo': 'Activo'}
+        fields = [
+            "codigo_personal",
+            "nombres",
+            "apellidos",
+            "fecha_nacimiento",
+            "cui",
+            "establecimiento",
+            "grado",
+            "imagen",
+            "tel",
+            "activo",
+        ]
         widgets = {
-            'grado': forms.Select(attrs={'class': 'form-control'}),
-            'imagen': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-            'tel': forms.TextInput(attrs={'class': 'form-control'}),
-            'activo': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            "fecha_nacimiento": forms.DateInput(attrs={"type": "date"}),
         }
 
+
+class EmpleadoEditForm(EmpleadoForm):
+    pass
+
+
+class EstablecimientoForm(BaseRihoForm):
+    class Meta:
+        model = Establecimiento
+        fields = ["nombre", "direccion", "background_gafete", "gafete_ancho", "gafete_alto", "activo"]
+
+
+class CarreraForm(BaseRihoForm):
+    class Meta:
+        model = Carrera
+        fields = ["establecimiento", "nombre", "activo"]
+
+
+class GradoForm(BaseRihoForm):
+    class Meta:
+        model = Grado
+        fields = ["carrera", "nombre", "descripcion", "jornada", "seccion", "activo"]
+
+
+class MatriculaForm(BaseRihoForm):
+    class Meta:
+        model = Matricula
+        fields = ["alumno", "grado", "ciclo", "estado"]
+
     def __init__(self, *args, **kwargs):
+        establecimiento_id = kwargs.pop("establecimiento_id", None)
+        carrera_id = kwargs.pop("carrera_id", None)
         super().__init__(*args, **kwargs)
-
-        # Personalizar los campos para agregar la clase 'form-control' en todos los campos si no está especificado
-        for field in self.fields.values():
-            if 'class' not in field.widget.attrs:
-                field.widget.attrs['class'] = 'form-control'
-
-        # Personalizar el queryset del campo 'grado' para asegurar que solo se muestren los grados disponibles
-        self.fields['grado'].queryset = Grado.objects.all()
+        alumnos = Empleado.objects.all()
+        grados = Grado.objects.select_related("carrera", "carrera__establecimiento")
+        if establecimiento_id:
+            alumnos = alumnos.filter(establecimiento_id=establecimiento_id)
+            grados = grados.filter(carrera__establecimiento_id=establecimiento_id)
+        if carrera_id:
+            grados = grados.filter(carrera_id=carrera_id)
+        self.fields["alumno"].queryset = alumnos.order_by("apellidos", "nombres")
+        self.fields["grado"].queryset = grados.order_by("nombre")
