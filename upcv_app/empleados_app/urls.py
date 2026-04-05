331eebd07c9bcc7e62b68411dad24d0fe26b6771
jrodash2 <61074808+jrodash2@users.noreply.github.com>
2026-04-04 19:54:41 -0600

    Fix gafete visibility consistency across editor preview and download

--- upcv_app/empleados_app/urls.py
+++ upcv_app/empleados_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("signin/", views.signin, name="signin"),
     path("logout/", views.signout, name="logout"),
     path("dahsboard/", views.dahsboard, name="dahsboard"),
+    path("establecimientos/<int:establecimiento_id>/dashboard/", views.dashboard_establecimiento, name="dashboard_establecimiento"),
     path("config_general/", views.configuracion_general, name="configuracion_general"),
     path("usuarios/", views.usuarios_list, name="usuarios_list"),
     path("usuarios/nuevo/", views.usuarios_create, name="usuarios_create"),
@@ -21,6 +22,7 @@ urlpatterns = [
     path("alumnos/lista/<int:e_id>/", views.editar_empleado, name="editar_empleado"),
     path("alumnos/credencial/", views.credencial_empleados, name="empleado_credencial"),
     path("alumnos/<int:id>/", views.empleado_detalle, name="empleado_detalle"),
+    path("alumnos/<int:id>/boleta-asistencia/", views.empleado_boleta_asistencia, name="empleado_boleta_asistencia"),
 
     path("establecimientos/", views.lista_establecimientos, name="establecimiento_lista"),
     path("establecimientos/crear/", views.crear_establecimiento, name="crear_establecimiento"),
@@ -30,8 +32,16 @@ urlpatterns = [
 
     path("matriculas/<int:matricula_id>/gafete.jpg", views.gafete_jpg, name="gafete_jpg"),
     path("matriculas/<int:matricula_id>/gafete_descarga.jpg", views.descargar_gafete_jpg, name="descargar_gafete_jpg"),
+    path("matriculas/<int:matricula_id>/gafete_descarga_frente.jpg", views.descargar_gafete_frente_jpg, name="descargar_gafete_frente_jpg"),
+    path("matriculas/<int:matricula_id>/gafete_descarga_reverso.jpg", views.descargar_gafete_reverso_jpg, name="descargar_gafete_reverso_jpg"),
     path("matriculas/masiva/", views.matricula_masiva, name="matricula_masiva"),
     path("matriculas/masiva/buscar/", views.matricula_masiva_buscar_alumnos, name="matricula_masiva_buscar_alumnos"),
+    path("academico/carga-masiva/", views.carga_masiva_home, name="carga_masiva_home"),
+    path("academico/carga-masiva/alumnos/", views.carga_masiva_import_alumnos, name="carga_masiva_import_alumnos"),
+    path("academico/carga-masiva/docentes/", views.carga_masiva_import_docentes, name="carga_masiva_import_docentes"),
+    path("academico/carga-masiva/cursos/", views.carga_masiva_import_cursos, name="carga_masiva_import_cursos"),
+    path("academico/carga-masiva/asignaciones/", views.carga_masiva_import_asignaciones, name="carga_masiva_import_asignaciones"),
+    path("academico/carga-masiva/plantillas/<str:tipo>/", views.carga_masiva_plantilla, name="carga_masiva_plantilla"),
 
     path("establecimientos/<int:establecimiento_id>/gafete/editor/", views.editor_gafete, name="editor_gafete"),
     path("establecimientos/<int:establecimiento_id>/gafete/diseno/guardar/", views.guardar_diseno_gafete, name="guardar_diseno_gafete"),
@@ -44,6 +54,7 @@ urlpatterns = [
     path("lista/<int:e_id>/", views.editar_empleado, name="legacy_editar"),
     path("credencial/", views.credencial_empleados, name="legacy_credencial"),
     path("empleado/<int:id>/", views.empleado_detalle, name="legacy_detalle"),
+    path("empleado/<int:id>/boleta-asistencia/", views.empleado_boleta_asistencia, name="legacy_boleta_asistencia"),
 
     path("", views.home, name="home"),
 ]
